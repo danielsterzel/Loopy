87260143b17f2f442ab13590856aa44389201d22
daniel <daniel.j.sterzel@gmaik.com>
2026-02-03 01:22:28 +0100

    API for front

--- src/main/java/com/api/PlayerController.java
+++ src/main/java/com/api/PlayerController.java
@@ -1,21 +1,35 @@
 package com.api;
 
 import com.spotify.client.SpotifyApiClient;
+import com.spotify.model.PlayerState.PlayerState;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestController
 @RequestMapping("/api/player") // prefix for all mappings in controller
 public class PlayerController {
     private final SpotifyApiClient spotifyApiClient;
+    private final ResponseEntityExceptionHandler responseEntityExceptionHandler;
 
-    public PlayerController(SpotifyApiClient spotifyApiClient){
+    public PlayerController(SpotifyApiClient spotifyApiClient, ResponseEntityExceptionHandler responseEntityExceptionHandler){
         this.spotifyApiClient = spotifyApiClient;
+        this.responseEntityExceptionHandler = responseEntityExceptionHandler;
     }
     @GetMapping("/raw")
     public String rawPlayerJson(){
         return spotifyApiClient.getCurrentPlayerRawJson();
+    }
+
+
+    // ResponseEntity == full HTTP response with error code body and headers
+    @GetMapping
+    public ResponseEntity<PlayerState> getPlayer()
+    {
+        return spotifyApiClient.getCurrentPlayer().map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.noContent().build());
     }
 }
 
